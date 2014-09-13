6407c46e8be1eb360a9748292bd4a3625f44bdd1
cmwelsh <chris@cmwelsh.com>
2014-09-13 11:27:02 -0400

    Add full screen Esri Leaflet map

--- htdocs/scripts/src/application.js
+++ htdocs/scripts/src/application.js
@@ -2,8 +2,10 @@ define(function(require) {
     'use strict';
 
     var $ = require('jquery');
+    var L = require('leaflet');
     var moment = require('moment');
     var _ = require('underscore');
+    require('esri-leaflet');
 
     var log = require('lib/logger');
 
@@ -15,6 +17,10 @@ define(function(require) {
         $('#test').text('Hello world!');
         log.info(_.extend({}, {}));
         log.info(moment().format());
+
+        var map = L.map('map').setView([35.221646, -80.845350], 13);
+        L.esri.basemapLayer('Streets', {detectRetina: true}).addTo(map);
+        map.locate({setView: true, maxZoom: 16});
     };
 
     return Application;
--- htdocs/scripts/src/config.js
+++ htdocs/scripts/src/config.js
@@ -2,7 +2,9 @@
 
 requirejs.config({
     paths: {
+        'esri-leaflet': '/components/esri-leaflet/index',
         jquery: '/components/jquery/dist/jquery',
+        leaflet: '/components/leaflet/index',
         loglevel: '/components/loglevel/dist/loglevel',
         moment: '/components/moment/moment',
         underscore: '/components/underscore/underscore'
